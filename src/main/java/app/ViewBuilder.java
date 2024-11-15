d52afc88b836eafa43a761dbccab8e6a7b89e149
anthonymountain <anthonyshan08@gmail.com>
2024-11-15 01:39:07 -0700

    Fixed ViewBuilder.java Now takes calls with proper params from AppBuilder.java
    Signed-off-by: anthonymountain <anthonyshan08@gmail.com>

--- src/main/java/app/ViewBuilder.java
+++ src/main/java/app/ViewBuilder.java
@@ -1,22 +1,13 @@
 package app;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 
-import data_access.InMemoryUserDataAccessObject;
-import entity.CommonSongFactory;
-import entity.CommonUserFactory;
-import entity.SongFactory;
-import entity.UserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.change_password.LoggedInViewModel;
 import interface_adapter.login.LoginViewModel;
 import interface_adapter.rec_song.RecSongViewModel;
 import interface_adapter.signup.SignupViewModel;
 import view.*;
-
-import java.awt.*;
 
 /**
  * The ViewBuilder class is responsible for putting together the pieces of
@@ -25,68 +16,54 @@ import java.awt.*;
  * This is done by adding each View.
  */
 public class ViewBuilder {
-    private final JPanel cardPanel = new JPanel();
-    private final CardLayout cardLayout = new CardLayout();
-    private final SongFactory songFactory = new CommonSongFactory();
-    private final UserFactory userFactory = new CommonUserFactory();
-    private final ViewManagerModel viewManagerModel = new ViewManagerModel();
-    private final ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+    private final JPanel cardPanel;
+    private final ViewManagerModel viewManagerModel;
 
-    private final InMemoryUserDataAccessObject userDataAccessObject = new InMemoryUserDataAccessObject();
-
-    private SignupView signupView;
-    private SignupViewModel signupViewModel;
-    private LoginViewModel loginViewModel;
-    private LoggedInViewModel loggedInViewModel;
-    private LoggedInView loggedInView;
-    private LoginView loginView;
-    private RecSongViewModel recSongViewModel;
-    private RecSongView recSongView;
-
-    public ViewBuilder() {
-        cardPanel.setLayout(cardLayout);
+    public ViewBuilder(JPanel cardPanel, ViewManagerModel viewManagerModel) {
+        this.cardPanel = cardPanel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     /**
      * Adds the Signup View to the application.
+     * @param signupViewModel a view model
      * @return this builder
      */
-    public SignupView createSignupView() {
-        signupViewModel = new SignupViewModel();
-        signupView = new SignupView(signupViewModel);
+    public SignupView createSignupView(SignupViewModel signupViewModel) {
+        final SignupView signupView = new SignupView(signupViewModel);
         cardPanel.add(signupView, signupView.getViewName());
         return signupView;
     }
 
     /**
-     * Adds the Login View to the application.
+     * Adds the Signup View to the application.
+     * @param loginViewModel a view model
      * @return this builder
      */
-    public LoginView createLoginView() {
-        loginViewModel = new LoginViewModel();
-        loginView = new LoginView(loginViewModel);
+    public LoginView createLoginView(LoginViewModel loginViewModel) {
+        final LoginView loginView = new LoginView(loginViewModel);
         cardPanel.add(loginView, loginView.getViewName());
         return loginView;
     }
 
     /**
-     * Adds the LoggedIn View to the application.
+     * Adds the Signup View to the application.
+     * @param loggedInViewModel a view model
      * @return this builder
      */
-    public LoggedInView createLoggedInView() {
-        loggedInViewModel = new LoggedInViewModel();
-        loggedInView = new LoggedInView(loggedInViewModel);
+    public LoggedInView createLoggedInView(LoggedInViewModel loggedInViewModel) {
+        final LoggedInView loggedInView = new LoggedInView(loggedInViewModel);
         cardPanel.add(loggedInView, loggedInView.getViewName());
         return loggedInView;
     }
 
     /**
-     * Adds the RecSong View to the application.
+     * Adds the Signup View to the application.
+     * @param recSongViewModel a view model
      * @return this builder
      */
-    public RecSongView createRecSongView() {
-        recSongViewModel = new RecSongViewModel();
-        recSongView = new RecSongView(recSongViewModel);
+    public RecSongView createRecSongView(RecSongViewModel recSongViewModel) {
+        final RecSongView recSongView = new RecSongView(recSongViewModel);
         cardPanel.add(recSongView, recSongView.getViewName());
         return recSongView;
     }
