0049f95d4e727e86f221529e6cc1864eaa2fa8e3
ponyopm <matteo.moretti@mail.utoronto.ca>
2024-11-15 16:49:29 -0500

    created Genre interface and CommonGenre entity

---
+++ src/main/java/entity/CommonGenre.java
@@ -0,0 +1,26 @@
+package entity;
+
+/**
+ * A simple implementation of the User interface.
+ */
+public class CommonGenre implements Genre {
+
+    private final String type;
+    private final String description;
+
+    public CommonGenre(String type, String description) {
+        this.type = type;
+        this.description = description;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+}
---
+++ src/main/java/entity/Genre.java
@@ -0,0 +1,21 @@
+package entity;
+
+/**
+ * The representation of a song in our program.
+ */
+public interface Genre {
+
+    /**
+     * Returns the name of the song.
+     *
+     * @return the name of the song.
+     */
+    String getType();
+
+    /**
+     * Returns the song genre.
+     * @return the song genre.
+     */
+    String getDescription();
+
+}
