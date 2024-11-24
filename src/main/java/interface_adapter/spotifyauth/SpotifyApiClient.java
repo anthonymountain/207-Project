72f32fc0ab5cf5efe70674c865f4f5bd44b29264
Descartes-AR <adwait.rath@mail.utoronto.ca>
2024-11-23 20:47:29 -0500

    fixed spotify auth

--- src/main/java/interface_adapter/spotifyauth/SpotifyApiClient.java
+++ src/main/java/interface_adapter/spotifyauth/SpotifyApiClient.java
@@ -1,5 +1,23 @@
-package SpotifyAuth;
+package interface_adapter.spotifyauth;
 
-public interface SpotifyApiClient {
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@Component
+public class SpotifyApiClient {
+
+    private final WebClient webClient;
+
+    public SpotifyApiClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://api.spotify.com/v1").build();
+    }
+
+    public String getUserProfile(String accessToken) {
+        return webClient.get()
+                .uri("/me")
+                .header("Authorization", "Bearer " + accessToken)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
