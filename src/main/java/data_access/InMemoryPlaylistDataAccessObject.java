097c581694390209d44dfe4b2bf36b06a8f88e41
dxuc <160444027+chengd35@users.noreply.github.com>
2024-11-29 11:12:33 -0500

    fixed Checkstyle errors in SpotifyAPIClient

--- src/main/java/data_access/InMemoryPlaylistDataAccessObject.java
+++ src/main/java/data_access/InMemoryPlaylistDataAccessObject.java
@@ -1,6 +1,7 @@
 package data_access;
 
 import entity.Playlist;
+import interface_adapter.spotify_auth.SpotifyApiClient;
 import use_case.rec_playlist.RecPlaylistDataAccessInterface;
 
 /**
@@ -12,7 +13,13 @@ public class InMemoryPlaylistDataAccessObject implements RecPlaylistDataAccessIn
 
     @Override
     public Playlist getRecommendations() {
+        final SpotifyApiClient spotifyApiClient = new SpotifyApiClient();
         // Make API call to get recommendation
+        final String recommendationsJSON = spotifyApiClient.getRecommendations("", "", "");
+
+        // Reformat the recommendations from JSON format into tracks that we can put in a playlist.
+
+
         final Playlist playlist1 = new Playlist();
         this.playlist = playlist1;
         // He has a point that the local variable playlist1 is useless.
