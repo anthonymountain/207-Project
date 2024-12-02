59b78389d07b1ca8b1291757ea2e6eb43dda9a17
riena42 <riena.asakawa@mail.utoronto.ca>
2024-12-02 17:57:02 -0500

    working on album service

--- src/main/java/services/AlbumService.java
+++ src/main/java/services/AlbumService.java
@@ -1,6 +1,9 @@
 package services;
 
 import entity.Album;
+
+import java.util.ArrayList;
+
 public class AlbumService {
     public ArrayList<Album> getNewReleases() {
         return null;
--- src/main/java/use_case/rec_album/RecAlbumDataAccessInterface.java
+++ src/main/java/use_case/rec_album/RecAlbumDataAccessInterface.java
@@ -2,8 +2,10 @@ package use_case.rec_album;
 
 import entity.Album;
 
+/**
+ * DAO for the Recommend Album Use Case.
+ */
 public interface RecAlbumDataAccessInterface {
-
         /**
         * Recommends an album for the user.
         */
