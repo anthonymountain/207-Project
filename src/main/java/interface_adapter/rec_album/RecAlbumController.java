de8f280a385e720aa9679eaf153c5ea6f9038899
riena42 <riena.asakawa@mail.utoronto.ca>
2024-12-02 18:10:16 -0500

    half way done

--- src/main/java/interface_adapter/rec_album/RecAlbumController.java
+++ src/main/java/interface_adapter/rec_album/RecAlbumController.java
@@ -1,4 +1,31 @@
 package interface_adapter.rec_album;
 
+import java.util.ArrayList;
+
+import use_case.rec_album.RecAlbumInputBoundary;
+import use_case.rec_album.RecAlbumInputData;
+
+/**
+ * Controller for the Recommend Album Use Case.
+ */
 public class RecAlbumController {
+    private final RecAlbumInputBoundary recAlbumInteractor;
+
+    public RecAlbumController(RecAlbumInputBoundary inputBoundary) {
+        this.recAlbumInteractor = inputBoundary;
+    }
+
+    /**
+     * Sends the RecAlbumInputData to the Interactor.
+     */
+    @SuppressWarnings({"checkstyle:LocalFinalVariableName", "checkstyle:SuppressWarnings"})
+    public void execute() {
+        final String id = "";
+        final String name = "";
+        final ArrayList<String> artists = null;
+        final String album_type = "";
+        final ArrayList<Object> images = null;
+
+        recAlbumInteractor.execute(new RecAlbumInputData(id, name, artists, album_type, images));
+    }
 }
--- src/main/java/interface_adapter/rec_album/RecAlbumPresenter.java
+++ src/main/java/interface_adapter/rec_album/RecAlbumPresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter.rec_album;
 
-public class RecAlbumPresenter {
+import use_case.rec_album.RecAlbumOutputBoundary;
+import use_case.rec_album.RecAlbumOutputData;
+
+/**
+ * Presenter for the Recommend Album Use Case.
+ */
+public class RecAlbumPresenter implements RecAlbumOutputBoundary {
+    @Override
+    public void prepareSuccessView(RecAlbumOutputData outputData) {
+        System.out.println("Recommended Album: " + outputData.getAlbumName());
+    }
+
+    @Override
+    public void prepareFailView(RecAlbumOutputData outputData) {
+        System.err.println("Error: " + outputData.getMessage());
+    }
 }
