c53796cd90ad942261747b7839cad0bba35fcefa
ponyopm <matteo.moretti@mail.utoronto.ca>
2024-12-03 19:19:59 -0500

    Refactored RecGenreInputData

--- src/main/java/use_case/rec_genre/RecGenreInputData.java
+++ src/main/java/use_case/rec_genre/RecGenreInputData.java
@@ -1,20 +1,13 @@
 package use_case.rec_genre;
 
-import java.util.ArrayList;
-
 /**
  * The Input Data for the Recommend Genre Use Case.
  */
 public class RecGenreInputData {
 
-    private final ArrayList<String> genres;
-
-    public RecGenreInputData(ArrayList<String> genres) {
-        this.genres = genres;
-    }
-
-    public ArrayList<String> getGenres() {
-        return this.genres;
+    public RecGenreInputData() {
+        // InputData has no attributes; we don't actually need
+        // any input data for this use case.
     }
 }
 
