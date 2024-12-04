c00aa51a94e663d3dfa83dca400b1c1c5c416a0f
riena42 <riena.asakawa@mail.utoronto.ca>
2024-12-03 19:37:57 -0500

    something done

--- src/main/java/view/RecAlbumView.java
+++ src/main/java/view/RecAlbumView.java
@@ -1,27 +1,39 @@
 package view;
 
+import entity.Album;
 import org.apache.tomcat.Jar;
 
 import javax.swing.*;
 import interface_adapter.rec_album.RecAlbumController;
 
-public class RecAlbumView {
+import java.awt.*;
+
+public class RecAlbumView extends JPanel {
+
+    private static final Color DARK_BACKGROUND = new Color(24, 24, 32);
+    private static final int TEN = 10;
+    private static final int FOUR_HUNDRED = 400;
+    private static final String FONT = "Futura";
+
+    private final ViewBuilder builder = new ViewBuilder();
 
     private final JPanel view;
-    private RecAlbumController recAlbumController;
+    private JButton recAlbum;
+    private Album album;
 
     public RecAlbumView() {
-        final ViewBuilder viewBuilder = new ViewBuilder();
-        viewBuilder.addLabel("Recommended Album");
-        viewBuilder.addLabel("placeholder_name");
-        this.view = viewBuilder.build();
+        builder.addLabel("Recommended Album:")
+                .addButton("recAlbum", "Recommend Album")
+                .setViewName("Recommended Album");
+        builder.setPreferredSize(FOUR_HUNDRED, FOUR_HUNDRED);
+
+        this.view = builder.build();
+        this.add(view);
+        this.setBackground(DARK_BACKGROUND);
+        view.setBorder(BorderFactory.createEmptyBorder(TEN, TEN, TEN, TEN));
     }
 
     public JPanel getView() {
         return view;
-    }
-
-    public void setRecAlbumController(RecAlbumController recAlbumController) {
-        this.recAlbumController = recAlbumController;
     }
 }
